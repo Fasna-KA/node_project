cb14437006a26f72db7f7c6c381854ab51034450
FASNA-KA <jasmeeazeez813@gmail.com>
2022-07-27 19:54:01 +0530

    cors

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 const express = require ("express")
 const app = express()
 const DataRouter = require("./Routes/data")
+const cors = require("cors")
 
 // app.get("/home", function (req,res) {
 
 //     res.send("Home")
 // })
 
+app.use(cors())
 app.use("/api",DataRouter)
 
 app.listen(process.env.PORT||8080,function () {
